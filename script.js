7b987c7981ecd2ddb604eba0e815c665253aff12
Wire-Ghost <evanpanta998@gmail.com>
2025-05-06 22:58:14 +0545

    YOUTUBE SIDE BAR DONE

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('menuToggle').addEventListener('click', () => {
+        document.getElementById('sidebar').classList.toggle('open');
+      });
+  });
+  
